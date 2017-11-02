9d16f38f5fae228c965eb20f440ad8d9c3e6a06a
ly2011 <1518550424@qq.com>
2017-11-02 23:45:13 +0800

    处理用户删除

--- src/services/users.js
+++ src/services/users.js
@@ -1,6 +1,14 @@
-import request from '../utils/request';
-import { PAGE_SIZE } from '../constants';
+import request from "../utils/request";
+import { PAGE_SIZE } from "../constants";
 
+// 获取用户列表
 export function fetch({ page }) {
   return request(`/api/users?_page=${page}&_limit=${PAGE_SIZE}`);
+}
+
+// 删除用户信息
+export function remove(id) {
+  return request(`/api/users/${id}`, {
+    method: "DELETE"
+  });
 }
