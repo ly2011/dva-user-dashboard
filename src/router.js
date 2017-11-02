308c2ccf3f754b24af99d2fd7489f69dd71d27e4
fengyun2 <2816708306@qq.com>
2017-11-02 15:28:41 +0800

    纯净版

--- src/router.js
+++ src/router.js
@@ -1,38 +1,13 @@
 import React from 'react';
-import { Router } from 'dva/router';
+import { Router, Route } from 'dva/router';
+import IndexPage from './routes/IndexPage';
 
-const cached = {};
-function registerModel(app, model) {
-  if (!cached[model.namespace]) {
-    app.model(model);
-    cached[model.namespace] = 1;
-  }
-}
-
-function RouterConfig({ history, app }) {
-  const routes = [
-    {
-      path: '/',
-      name: 'IndexPage',
-      getComponent(nextState, cb) {
-        require.ensure([], (require) => {
-          cb(null, require('./routes/IndexPage'));
-        });
-      },
-    },
-    {
-      path: '/users',
-      name: 'UsersPage',
-      getComponent(nextState, cb) {
-        require.ensure([], (require) => {
-          registerModel(app, require('./models/users'));
-          cb(null, require('./routes/Users'));
-        });
-      },
-    },
-  ];
-
-  return <Router history={history} routes={routes} />;
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Route path="/" component={IndexPage} />
+    </Router>
+  );
 }
 
 export default RouterConfig;
